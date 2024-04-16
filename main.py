196ace8b3ec341e3807407398faa3a2671793aa4
hubi360 <163434837+hubi360@users.noreply.github.com>
2024-04-16 18:17:23 +0200

    wip: add new manu function, add user append

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from models.data_source import users
-from utils.crud import read_friends
+from utils.crud import read_friends, add_user
+
+
 
 if __name__ == '__main__': #umieszcza się to żeby urochomic plik jest przypisany interpreter
     while True:
         print("Welcome to the menu choose an option: ")
-        print("1. Read a list of friends")
         print("0. Exit")
+        print("1. Read a list of friends")
+        print("2. Add new user")
         menu_option = input("Choose an option:")
         if menu_option == "0":
             break
         if menu_option == "1":
             read_friends(users)
+        if menu_option == "2":
+            add_user(users)
 
--- utils/crud.py
+++ utils/crud.py
@@ -3,3 +3,9 @@ def read_friends(users: list) -> None:
     for user in users:
         print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
 
+def add_user(lista: list) -> list:  # wezmie liste i potem odd liste
+    imie = input("Podaj imie: ")
+    nazwisko = input("Podaj nazwisko: ")
+    liczba_psotow = int(input("Podaj liczbe postow uzytkownika: "))
+    new_user = {"name": imie, "surname": nazwisko, "posts": liczba_psotow}
+    lista.append(new_user)
