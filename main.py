e678e48325791795786fa7daefbb0fbfa8bac685
hubi360 <163434837+hubi360@users.noreply.github.com>
2024-04-16 17:06:55 +0200

    wip: read code refactor to function

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ users: list = [
 
 ]
 
-
 def read_friends(users: list)->None:
     print("Informacje o twoich znajomych: ")
     for user in users:
         print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
 read_friends(users)
 
