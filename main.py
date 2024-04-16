2c9123b1d869957c0755c673e9921d2c4a25e397
hubi360 <163434837+hubi360@users.noreply.github.com>
2024-04-16 17:51:50 +0200

    wip: code refactor add menu

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-users: list = [
-    {"name": "Julia", "surname": "Gotowiec", "posts": 1500},
-    {"name": "Hubert", "surname": "Sybilski", "posts": 123456},
-    {"name": "Adrian", "surname": "Dobrzański", "posts": 3}
+from models.data_source import users
+from utils.crud import read_friends
 
-]
+if __name__ == '__main__': #umieszcza się to żeby urochomic plik jest przypisany interpreter
+    while True:
+        print("Welcome to the menu choose an option: ")
+        print("1. Read a list of friends")
+        print("0. Exit")
+        menu_option = input("Choose an option:")
+        if menu_option == "0":
+            break
+        if menu_option == "1":
+            read_friends(users)
 
-
-def read_friends(users: list) -> None:
-    print("Informacje o twoich znajomych: ")
-    for user in users:
-        print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
-
-
-read_friends(users)
