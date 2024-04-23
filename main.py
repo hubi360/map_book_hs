608e4640d610ca10ff0e9b37dda62aa93f4c7e1a
hubi360 <163434837+hubi360@users.noreply.github.com>
2024-04-23 20:10:38 +0200

    feat: add an interactive map

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ from models.data_source import users
 from utils.crud import read_friends, add_user, search_user, remove_user, update_user
 from utils.emap import single_map, full_map
 
-(users)
-
-if __name__ == '__main__':  # umieszcza się to żeby urochomic plik jest przypisany interpreter
+if __name__ == '__main__':
     while True:
-        print("Welcome to the menu choose an option: ")
-        print("0. Exit")
-        print("1. Read a list of friends")
+        print("Welcome to the menu  ")
+        print("0. Exit ")
+        print("1. Read a list of friends ")
         print("2. Add new user")
         print("3. Search user")
         print("4. Remove user")
         print("5. Update user")
-        print("6. Generate map")
+        print("6. Generate single map")
         print("7. Generate full map")
         menu_option = input("Choose an option:")
         if menu_option == "0":
--- utils/emap.py
+++ utils/emap.py
@@ -1,31 +1,28 @@
+import folium
 import requests
 from bs4 import BeautifulSoup
-import folium
-from models.data_source import users
-
 def single_map(user_location:str)->None:
     url: str = f'https://pl.wikipedia.org/wiki/{user_location}'
-    responsse = requests.get(url)
-    responsse_html = BeautifulSoup(responsse.text, 'html.parser')
-    latitude = responsse_html.select('.latitude')[1].text.replace(",",".")
-    longitude = responsse_html.select('.longitude')[1].text.replace(",",".")
+    response = requests.get(url)
+    response_html = BeautifulSoup(response.text, 'html.parser')
+    latitude = response_html.select('.latitude')[1].text.replace(",",".")
+    longitude = response_html.select('.longitude')[1].text.replace(",",".")
     map = folium.Map(location=[latitude, longitude], zoom_start=11)
-    folium.Marker(location=[latitude, longitude], popup=f"{user_location} rządzi !!??").add_to(map)
+    folium.Marker(location=[latitude, longitude],popup=f"{user_location} rządzi!!?? ").add_to(map)
     map.save(f'./{user_location}.html')
 
-for user in users:
-    single_map(user['location'])
 
 def full_map(users:str)->None:
-    lista_wsplrzednych = []
+    lista_wspolrzednych = []
     map = folium.Map(location=[52, 21], zoom_start=8)
     for user in users:
         url: str = f'https://pl.wikipedia.org/wiki/{user['location']}'
-        responsse = requests.get(url)
-        responsse_html = BeautifulSoup(responsse.text, 'html.parser')
-        latitude = responsse_html.select('.latitude')[1].text.replace(",",".")
-        longitude = responsse_html.select('.longitude')[1].text.replace(",",".")
-        lista_wsplrzednych.append([latitude, longitude])
-    for wsplrzedne in lista_wsplrzednych:
-        folium.Marker(location=wsplrzedne).add_to(map)
+        response = requests.get(url)
+        response_html = BeautifulSoup(response.text, 'html.parser')
+        latitude = response_html.select('.latitude')[1].text.replace(",",".")
+        longitude = response_html.select('.longitude')[1].text.replace(",",".")
+        lista_wspolrzednych.append([latitude, longitude])
+    for wspolrzedne in lista_wspolrzednych:
+        folium.Marker(location= wspolrzedne ).add_to(map)
+
     map.save(f'./common_map.html')
